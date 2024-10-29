7b06d5dfbcc824bc11bba9c83e0d040a052fe522
Stefano Bianda <stefanobianda@ik.me>
2024-10-29 13:41:11 +0100

    use of Route::resource

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,4 @@ use App\Http\Controllers\JobController;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{id}', [JobController::class, 'show']);
-
-Route::post('/jobs', [JobController::class, 'store']);
+Route::resource('jobs', JobController::class);
