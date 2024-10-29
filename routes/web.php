77ed9fb5a0ab6e146f7f8b3a1174c25fcb2c85d4
Stefano Bianda <stefanobianda@ik.me>
2024-10-29 13:35:18 +0100

    recreate job controller: php artisan make:controller JobController --resource

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@ Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/jobs', [JobController::class, 'index']);
 Route::get('/jobs/create', [JobController::class, 'create']);
+Route::get('/jobs/{id}', [JobController::class, 'show']);
+
+Route::post('/jobs', [JobController::class, 'store']);
