66bee7854f3bc8f7370c49f132dd0f5b47f873ef
Stefano Bianda <stefanobianda@ik.me>
2024-10-30 09:58:35 +0100

    activation of Vite

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$title ?? 'Workopia | Find and list jobs'}}</title>
 </head>
