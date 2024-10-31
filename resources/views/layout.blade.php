71a56568e82d0d1cfea1b30c9791d313515c3e46
Stefano Bianda <stefanobianda@ik.me>
2024-10-31 15:49:01 +0100

    add hero elements

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,13 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{asset('css/style.css')}}"
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$title ?? 'Workopia | Find and list jobs'}}</title>
 </head>
 <body class="bg-gray-100">
     <x-header/>
+    @if(request()->is('/'))
+    <x-hero/>
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{$slot}}
     </main>
+
+    <script src="{{asset('js/script.js')}}"></script>
 </body>
 </html>
