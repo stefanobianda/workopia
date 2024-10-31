214d9a40394086127f01b153a9b61886f6b9c2fc
Stefano Bianda <stefanobianda@ik.me>
2024-10-31 16:30:29 +0100

    include top and bottom banner

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,11 +12,13 @@
 <body class="bg-gray-100">
     <x-header/>
     @if(request()->is('/'))
-    <x-hero/>
+    <x-hero />
+    <x-top-banner />
     @endif
     <main class="container mx-auto p-4 mt-4">
         {{$slot}}
     </main>
+    
 
     <script src="{{asset('js/script.js')}}"></script>
 </body>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,3 +1,5 @@
 <x-layout>
 <h1>Welcome to Workopia</h1>
+
+<x-bottom-banner />
 </x-layout>
