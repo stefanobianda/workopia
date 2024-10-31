35a4db2ed745c0d184eab28757aff125f543c386
Stefano Bianda <stefanobianda@ik.me>
2024-10-31 11:21:44 +0100

    add button component and adjust navigation menu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,14 +4,12 @@
             <a href="{{url('/')}}">Workopia</a>
         </h1>
         <nav class="hidden md:flex items-center space-x-4">
-            <x-nav-link url='/jobs' :active="request()->is('jobs')">All Jobs 1</x-nav-link>
+            <x-nav-link url='/jobs' :active="request()->is('jobs')">All Jobs</x-nav-link>
             <x-nav-link url='/jobs/saved' :active="request()->is('jobs/saved')">Saved Jobs</x-nav-link>
             <x-nav-link url='/login' :active="request()->is('login')" icon="user">Login</x-nav-link>
             <x-nav-link url='/register' :active="request()->is('register')">Register</x-nav-link>
             <x-nav-link url='/dashboard' :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link>
-            <a href="{{url('/jobs/create')}}" class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300">
-                <i class="fa fa-edit"></i> Create Job
-            </a>
+            <x-button-link url="/jobs/create" icon="edit">Create Job</x-button-link>
         </nav>
         <button id="hamburger" class="text-white md:hidden flex items-center">
             <i class="fa fa-bars text-2xl"></i>
@@ -21,15 +19,11 @@
     <nav
         id="mobile-menu"
         class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
-        <a href="{{url('/jobs')}}" class="block px-4 py-2 hover:bg-blue-700">All Jobs</a>
-        <a href="{{url('/jobs/saved')}}" class="block px-4 py-2 hover:bg-blue-700">Saved Jobs</a>
-        <a href="{{url('/login')}}" class="block px-4 py-2 hover:bg-blue-700">Login</a>
-        <a href="{{url('/register')}}" class="block px-4 py-2 hover:bg-blue-700">Register</a>
-        <a href="{{url('/dashboard')}}" class="block text-white hover:underline py-2">
-            <i class="fa fa-gauge mr-1"></i> Dashboard
-        </a>
-        <a href="{{url('/jobs/create')}}" class="block px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-black">
-            <i class="fa fa-edit"></i> Create Job
-        </a>
+        <x-nav-link url='/jobs' :active="request()->is('jobs')" :mobile="true">All Jobs</x-nav-link>
+        <x-nav-link url='/jobs/saved' :active="request()->is('jobs/saved')" :mobile="true">Saved Jobs</x-nav-link>
+        <x-nav-link url='/login' :active="request()->is('login')" icon="user" :mobile="true">Login</x-nav-link>
+        <x-nav-link url='/register' :active="request()->is('register')" :mobile="true">Register</x-nav-link>
+        <x-nav-link url='/dashboard' :active="request()->is('dashboard')" icon="gauge" :mobile="true">Dashboard</x-nav-link>
+        <x-button-link url="/jobs/create" icon="edit" :block="true">Create Job</x-button-link>
     </nav>
 </header>
