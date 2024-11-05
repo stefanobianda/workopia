82a494e9e0d3ae2570c0c85b3a48fbf8590a0acb
Stefano Bianda <stefanobianda@ik.me>
2024-11-05 13:39:33 +0100

    Home page update, adding latest 3 jobs and some details

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Job;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-        return view('pages.index');
+        $jobs = Job::latest()->limit(3)->get();
+        return view('pages.index')->with('jobs', $jobs);
     }
 }
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <h1 class="text-2xl">All Jobs</h1>
 
-    <div class="grid grid-cols-1 md:grid-cols-3 gap4 mb-6">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         @forelse($jobs as $job)
         <x-job-card :job="$job" />
         @empty
