a47266b45339beeb3b32b64f130efcbb7e4fe895
Stefano Bianda <stefanobianda@ik.me>
2024-11-07 16:43:49 +0100

    registration is now implemented

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 
 class RegisterController extends Controller
@@ -12,5 +15,23 @@ class RegisterController extends Controller
     public function register(): View
     {
         return view('auth.register');
+    }
+
+    // @desc Store user in database
+    // @route POST /register
+    public function store(Request $request): RedirectResponse
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        // Hash pasword
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        // Create user
+        $user = User::create($validatedData);
+        return redirect()->route('login')->with('success', 'You are registered and can login!');
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,19 @@
 <x-layout>
-    Register
+    <div class="bg-white rounded-lg shadow-md w-full md:max-w-xl mx-auto mt-12 p-8 py-12">
+        <h2 class="text-4xl text-center font-bold mb-4">Register</h2>
+        <form method="POST" action="{{route('register.store')}}">
+            @csrf
+            <x-inputs.text id="name" name="name" placeholder="Full Name" />
+            <x-inputs.text id="email" name="email" placeholder="Email address" />
+            <x-inputs.text id="password" name="password" type="password" placeholder="Password" />
+            <x-inputs.text id="password_confirmation" name="password_confirmation" type="password" placeholder="Confirm Password" />
+
+            <button type="submit" class="w-full bg-blue-600 text-white px-4 py-2 rounded focus:outline-none">
+                Register
+            </button>
+            
+            <p class
+        </form>
+    </div>
+
 </x-layout>
