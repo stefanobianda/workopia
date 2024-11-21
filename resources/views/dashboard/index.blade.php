2c856091c2da031f9c9f49a255d64df3b64ebd04
Stefano Bianda <stefanobianda@ik.me>
2024-11-21 14:11:39 +0100

    applicant info shown on the profile / dashboard page

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -25,32 +25,60 @@
         </div>
         {{-- Job Listings --}}
         <div class="bg-white p-8 rounded-lg shadow-md w-full">
-        <h3 class="text-3xl text-center font-bold mb-4">
-            My Job Listings
-        </h3>
-        @forelse ($jobs as $job)
-            <div class="flex justify-between items-center border-b-2 border-gray-200 py-2">
-                <div>
-                    <h3 class="text-xl font-semibold">{{$job->title}}</h3>
-                    <p class="text-gray-700">{{$job->job_type}}</p>
+            <h3 class="text-3xl text-center font-bold mb-4">
+                My Job Listings
+            </h3>
+            @forelse ($jobs as $job)
+                <div class="flex justify-between items-center border-b-2 border-gray-200 py-2">
+                    <div>
+                        <h3 class="text-xl font-semibold">{{$job->title}}</h3>
+                        <p class="text-gray-700">{{$job->job_type}}</p>
+                    </div>
+                    <div class="flex space-x-3">
+                    <a href="{{route('jobs.edit', $job->id)}}" class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
+                    <!-- Delete Form -->
+                    <form method="POST" action="{{route('jobs.destroy', $job->id)}}?from=dashboard" onsubmit="return confirm('Are you sure that you want to delete this job?')">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded" >
+                            Delete
+                        </button>
+                    </form>
+                    <!-- End Delete Form -->
+                    </div>
                 </div>
-                <div class="flex space-x-3">
-                <a href="{{route('jobs.edit', $job->id)}}" class="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">Edit</a>
-                <!-- Delete Form -->
-                <form method="POST" action="{{route('jobs.destroy', $job->id)}}?from=dashboard" onsubmit="return confirm('Are you sure that you want to delete this job?')">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="px-4 py-2 bg-red-500 hover:bg-red-600 text-white rounded" >
-                        Delete
-                    </button>
-                </form>
-                <!-- End Delete Form -->
+
+                {{-- Applicants --}}
+                <div class="mt-4">
+                    <h4 class="text-lg font-semibold mb-2">Applicants</h4>
+                    @forelse ($job->Applicants as $applicant)
+                        <div class="py-2">
+                            <p class="text-gray-800">
+                                <strong>Name: </strong> {{$applicant->full_name}}
+                            </p>
+                            <p class="text-gray-800">
+                                <strong>Phone: </strong> {{$applicant->contact_phone}}
+                            </p>
+                            <p class="text-gray-800">
+                                <strong>Email: </strong> {{$applicant->contact_email}}
+                            </p>
+                            <p class="text-gray-800">
+                                <strong>Message: </strong> {{$applicant->message}}
+                            </p>
+                            <p class="text-gray-800 my-4">
+                                <a href="{{asset('storage/' . $applicant->resume_path)}}" class="text-blue-500 hover:underline" download>
+                                    <i class="fas fa-download"></i> Download Resume
+                                </a>
+                            </p>
+                        </div>
+                    @empty
+                        <p class="text-gray-700">No applicants for this job</p>
+                    @endforelse
                 </div>
-    </div>
-        @empty
-            <p class="text-gray-700">You have not job listings</p>
-        @endforelse
-    </div>
+            @empty
+                <p class="text-gray-700">You have not job listings</p>
+            @endforelse
+        </div>
     </section>
     <x-bottom-banner />
 </x-layout>
