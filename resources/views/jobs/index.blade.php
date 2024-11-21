cdb6732a91f19231d31c4a0b964b31079ffb3716
Stefano Bianda <stefanobianda@ik.me>
2024-11-21 21:30:53 +0100

    execute query statement and add back button

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,7 +2,13 @@
     <div class="bg-blue-900 h-24 px-4 mb-4 flex justify-center items-center rounded">
         <x-search />
     </div>
-    <h1 class="text-2xl">All Jobs</h1>
+
+    {{-- Back button--}}
+    @if (request()->has('keywords') || request()->has('location'))
+        <a href="{{route('jobs.index')}}" class="bg-gray-600 hover:bg-gray-700 text-white px-4 py-2 rounded mb-4 inline-block">
+            <i class="fa fa-arrow-left mr-1"></i> Back
+        </a>
+    @endif
 
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         @forelse($jobs as $job)
